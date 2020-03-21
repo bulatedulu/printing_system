6835ae225da2dfa82ef61527e3ea3c42bfafa4e1
bulatedulu <lhg171453@163.com>
2020-03-21 15:06:07 +0800

    打印系统-流程管控项目创建

---
+++ process_control/src/main/java/com/lhg/process_control/ProcessControlApplication.java
@@ -0,0 +1,13 @@
+package com.lhg.process_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessControlApplication.class, args);
+    }
+
+}
